230befd2a79f7176824c7b02144fffb473a1f520
joseph quintiliano <jquintiliano1@gmail.com>
2023-11-29 16:18:33 -0500

    Inital commit

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+import colors from 'tailwindcss/colors'
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))', // ADDS RADIAL GRADIENT 
+      },
+      colors: {
+        secondary: {
+          DEFAULT: colors.neutral[200],
+          hover: colors.neutral[300],
+          border: colors.neutral[400],
+          text: colors.neutral[500],
+          dark: colors.neutral[800],
+          ['dark-hover']: colors.neutral[900]
+        }
+      }
+    },
+  },
+  plugins: [],
+}
+
