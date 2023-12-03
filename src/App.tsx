8dbee8dc7885a0bd7711acafc44fc0973423314e
joseph quintiliano <jquintiliano1@gmail.com>
2023-12-03 12:00:07 -0500

    fetched data commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,62 +1,71 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import UserInput from "./components/userInput";
+import { ResultRow } from "./components/ResultRow";
 
-import { useEffect, useState } from 'react'
-import './App.css'
-import UserInput from './components/userInput'
-import { ResultRow } from './components/ResultRow'
+// type TCachedValues = {
+//   [keys: string]: object | void
+// };
 
 function App() {
-  const [userInput, setUserInput] = useState('')
-  const [cachedResults, setCachedResults] = useState([])
+  const [userInput, setUserInput] = useState("");
+  const [cachedResults, setCachedResults] = useState([]);
 
   useEffect(() => {
-    async function fetchCachedResults(){
-      try{
-       const res = await fetch("https://ambeigj4hx.us.aircode.run/cachedValues")
-       if(!res.ok){
-         throw new Response('Non 200 HTTP Status', res)
-       }
-       const data = await res.json()
-       setCachedResults(data)
-      }catch(err: any){
-        switch (err?.response.status){
-          case 400: break;
-          case 401: break;
-          case 404: break;
-          case 500: break;
+    const fetchCachedResults = async () => {
+      try {
+        const res = await fetch(
+          "https://ambeigj4hx.us.aircode.run/cachedValues"
+        );
+        if (!res.ok) {
+          throw new Response("Non 200 HTTP Status", res);
         }
-        handle(err)
-        throw(err)
+        const data = await res.json();
+        setCachedResults(data)
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      } catch (err: any) {
+        switch (err.status) {
+          case 400:
+            break;
+          case 401:
+            break;
+          case 404:
+            break;
+          case 500:
+            break;
+        }
+        console.log(err);
       }
-      return() => {
-
-      }
-
+      
+      return () => {};
     }
 
     fetchCachedResults();
-
-
-  }, [])
+    
+  }, []);
+  
   console.log(cachedResults)
+  console.log(typeof cachedResults)
+
   return (
     <>
-      <div className='h-screen text-white bg-gradient-radial from-[#2e4363] via-[#1e3258] to-[#111c33]'>
-        <main className='mx-auto px-4 py-8 max-w-2xl'>
-          <h1 className='font-bold text-6xl text-center'>NFL STAT TRACKER</h1>
-          <div className='flex justify-center mt-8'>
-            <UserInput className="" value={userInput} onChange={e => setUserInput(e.target.value)}/>
+      <div className="h-screen text-white bg-gradient-radial from-[#2e4363] via-[#1e3258] to-[#111c33]">
+        <main className="mx-auto px-4 py-8 max-w-2xl">
+          <h1 className="font-bold text-6xl text-center">NFL STAT TRACKER</h1>
+          <div className="flex justify-center mt-8">
+            <UserInput
+              className=""
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
           </div>
-          <div className='mt-8'>
-            <ResultRow results={cachedResults} userInput={userInput}/>
+          <div className="mt-8">
+            <ResultRow results={cachedResults} userInput={userInput} />
           </div>
         </main>
       </div>
     </>
-  )
+  );
 }
 
-export default App
-function handle(err: any) {
-  throw new Error('Function not implemented.')
-}
-
+export default App;
--- src/components/ResultRow.tsx
+++ src/components/ResultRow.tsx
@@ -1,45 +1,63 @@
 import { useState } from "react";
 import { Stats } from "./Stats";
 
-type TcachedValues = {
-  [keys: string]: object | string;
-};
+
 
 export const ResultRow = ({
-    cachedResults, userInput
-}: TcachedValues) => {
-  const [statsOpen, setStatsOpen] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+    results, userInput
+}) => {
 
-//   function searchCachedResults(userInput: string): <TcachedValues> {
-//     if(userInput.toLowerCase() =)
-//   }
-
-
-
-
-  return (
-    <div className="border relative overflow-hidden rounded-md border-white/30 px-4 py-2">
-      <div
-        className=" h-8 flex justify-between"
-        onClick={() => {
-          setStatsOpen(!statsOpen);
-        }}
-      >
-        {isLoading ? (
-          ""
-        ) : (
-          <>
-            <div>{cachedResults}</div>
-            <div>Team</div>
-            <div>Status</div>
-          </>
-        )}
-      </div>
-      {isLoading && (
-        <div className="skeleton-animation inset-0 bg-gradient-to-r from-transparent via-red-900 to-transparent absolute"></div>
-      )}
-      {!isLoading && statsOpen && <Stats />}
-    </div>
+    
+    
+    return (
+        <div className="border relative overflow-hidden rounded-md border-white/30 px-4 py-2">
+            {/* {results.map((team: string, key: string) => {
+               return (
+                <div key={key}>
+                    <h1>${team}</h1>
+                </div>
+               )
+            })} */}
+   
+        </div>
   );
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   const [statsOpen, setStatsOpen] = useState(false);
+//   const [isLoading, setIsLoading] = useState(false);
+{/* <div
+  className=" h-8 flex justify-between"
+  onClick={() => {
+    setStatsOpen(!statsOpen);
+  }}
+>
+  {isLoading ? (
+    ""
+  ) : (
+    <>
+      <div>name</div>
+      <div>Team</div>
+      <div>Status</div>
+    </>
+  )}
+</div>
+{isLoading && (
+  <div className="skeleton-animation inset-0 bg-gradient-to-r from-transparent via-red-900 to-transparent absolute"></div>
+)}
+{!isLoading && statsOpen && <Stats />} */}
